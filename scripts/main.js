91c926d8588e862e770585813cd6419ff406fea2
Nathael Arki <nathael.arki@supinternet.fr>
2017-11-14 20:33:38 +0100

    add delete task

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+{
+    'use strict'
+
+    new Vue({
+        el : '#app',
+        data : {
+            newTitle : '',
+            tasks : [
+                { title: 'Declencher Damien', isDone: true },
+                { title: 'Embeter Cécile', isDone: true },
+                { title: 'Manger Papou', isDone: false }
+            ]
+        },
+        methods: {
+            deleteTask(i) {
+                return this.tasks.splice(i, 1)
+            },
+            addTask(title) {
+                return this.tasks.push({title: title, isDone: false})
+            }
+        },
+        computed: {
+            remainingTasks(){
+                return this.tasks.filter(task => !task.isDone)
+            }
+        }
+    })
+}
