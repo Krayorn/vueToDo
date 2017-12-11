73ce9ec0b2595b32d1cf0dfc8b344da9282104b8
Nathael Arki <nathael.arki@supinternet.fr>
2017-12-11 14:09:08 +0100

    use components

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,58 @@
 {
     'use strict'
 
+    Vue.component('todo-item', {
+        props : ['task', 'index'],
+        template:   `<li class="collection-item">
+                        <input v-model='task.isDone' type="checkbox" :id="index" :checked="task.isDone">
+                        <label :for="index">{{ task.title }}</label>
+                        <a href="#" @click.prevent="deleteTask(index)" class="link-delete" title="Supprimer cette tâche">
+                            <i class="small material-icons">delete_forever</i>
+                        </a>
+                    </li>`,
+        methods: {
+            deleteTask(i) {
+                this.$emit('event-supress', i)
+            }
+        }
+    })
+
+    Vue.component('add-task', {
+        data : function() {
+            return {
+                newTaskTitle : ''
+            }
+        },
+        template:   `<fieldset>
+                        <form v-on:submit.prevent="addTask" class="row">
+                            <div class="input-field col m6 offset-m3">
+                                <input v-model='newTaskTitle' type="text">
+                                <label for="taskTitle">Intitulé de la tâche</label>
+                            </div>
+                            <div class="col m4 offset-m5">
+                                <button :disabled="newTaskTitle == ''" class="waves-effect waves-light btn">Ajouter</button>
+                            </div>
+                        </form>
+                    </fieldset>`,
+        methods : {
+            addTask() {
+                this.$emit('event-add-task', this.newTaskTitle)
+                this.newTaskTitle = ''
+            }
+        }
+    })
+
     new Vue({
         el : '#app',
         data : {
-            newTaskTitle : '',
             tasks : JSON.parse(localStorage.getItem('data')) || []
         },
         methods: {
             deleteTask(i) {
                 this.tasks.splice(i, 1)
             },
-            addTask() {
-                this.tasks.push({title: this.newTaskTitle, isDone: false})
-                this.newTaskTitle = ""
+            addTask(newTaskTitle) {
+                this.tasks.push({title: newTaskTitle, isDone: false})
             }
         },
         computed: {
