746d67131d28ee6f230dca8d8133a8d82872abad
Nathael Arki <nathael.arki@supinternet.fr>
2017-12-19 11:44:32 +0100

    add some webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
     entry : "./scripts/main.js",
     output : {
         filename: 'bundle.js',
         path : path.join(__dirname, "build")
-    }
+    },
+    plugins : [
+        new UglifyJsPlugin({
+            test: /^bundle\.js$/
+        })
+    ]
 }
